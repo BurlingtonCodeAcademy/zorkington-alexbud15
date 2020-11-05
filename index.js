696eaa1a7f4ec3ba624a5b84e029f456c7226ffa
Alex Budney <alexbudney@gmail.com>
2020-11-05 13:16:17 -0500

    update

--- index.js
+++ index.js
@@ -11,20 +11,46 @@ function ask(questionText) {
 }
 
 start();
-
+// start game
 async function start() {
   const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign.
 >_`;
+  // whatever typed, return 'you typed' and 'answer' and
+  // and sorry i don't know what that means
   let answer = await ask(welcomeMessage);
   console.log("You typed: " + answer);
-  console.log("Sorry, I don't know what " + answer + " means. goodbye");
+  console.log("Sorry, I don't know what " + answer + " means. ");
+
+  // when you type anything but exit, return same as above
+  while (answer !== "exit") {
+    answer = await ask(">_ ");
+    console.log("You typed: " + answer);
+    console.log("Sorry, I don't know what " + answer + " means. ");
+  }
+
+  }
+
+  answer = await ask(">_ ");
   process.exit();
 }
 
+
+
+
+
+
 /* 
+
+SIGN HINT
+  while (answer === "read sign") {
+    let sign = {
+      description: "Welcome to Burlington Code Academy! Come on up to the third floor.\nIf the door is locked, use the code 12345."
+    read: () => {
+      return this.description
+    }
 
 don't be afraid to commit or push, which you can do as many times as you want
 
